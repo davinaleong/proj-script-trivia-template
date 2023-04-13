300ceb2c4a84235766bf394d42d690c52fa3d1bf
Davina Leong <leong.shi.yun@gmail.com>
2023-04-13 18:07:53 +0800

    Done with image modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,14 @@ const classDNone = `d-none`
 
 const dataElementAttr = `data-element`
 const dataActiveAttr = `data-active`
+const srcAttr = `src`
 
-const alertEls = document.querySelectorAll(`[data-element="alert"]`)
-const btnOptionEls = document.querySelectorAll(`[data-element="btn-option"]`)
+const alertEls = document.querySelectorAll(getDataElementAttr(`alert`))
+const btnOptionEls = document.querySelectorAll(getDataElementAttr(`btn-option`))
+const imageModalEl = document.querySelector(getDataElementAttr(`image-modal`))
 
 alertEls.forEach(function (alertEl) {
-    const btnCloseAlertEl = alertEl.querySelector(`[${dataElementAttr}="btn-close-alert"]`)
+    const btnCloseAlertEl = alertEl.querySelector(getDataElementAttr(`btn-close-alert`))
 
     if (btnCloseAlertEl) {
         btnCloseAlertEl.addEventListener(`click`, function (event) {
@@ -27,3 +29,41 @@ btnOptionEls.forEach(function (btnOptionEl) {
         btnOptionEl.toggleAttribute(dataActiveAttr, true)
     })
 })
+
+if (imageModalEl) {
+    imageModalEl.removeAttribute(dataActiveAttr)
+
+    const btnCloseModalEl = imageModalEl.querySelector(getDataElementAttr(`btn-close-modal`))
+    btnCloseModalEl.addEventListener(`click`, function (event) {
+        event.preventDefault()
+        imageModalEl.removeAttribute(dataActiveAttr)
+    })
+
+    const modalImgEl = imageModalEl.querySelector(getDataElementAttr(`image-modal-img`))
+    const imgEls = document.querySelectorAll(`img`)
+    imgEls.forEach(function (imgEl) {
+        imgEl.addEventListener(`click`, function(event) {
+            const src = imgEl.getAttribute(srcAttr)
+            modalImgEl.setAttribute(srcAttr, src)
+
+            imageModalEl.setAttribute(dataActiveAttr, true)
+        })
+    })
+}
+
+/// Functions
+function getDataElementAttr(name) {
+    return `[${dataElementAttr}="${name}"]`
+}
+
+function toggleElement(element, toggle = false) {
+    if (element) {
+        if (toggle) {
+            element.classList.add(classActive)
+            element.setAttribute(dataActiveAttr, true)
+        } else {
+            element.classList.remove(classActive)
+            element.removeAttribute(dataActiveAttr)
+        }
+    }
+}
