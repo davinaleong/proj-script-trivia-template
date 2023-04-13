a4a20ca10d9ce079c21c0ac754ca5c17ce5b1ee5
Davina Leong <leong.shi.yun@gmail.com>
2023-04-13 14:29:13 +0800

    Done with alerts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('main.js loaded')
+
+const classDNone = `d-none`
+const dataElementAttr = `data-element`
+
+const alertEls = document.querySelectorAll(`[data-element="alert"]`)
+
+alertEls.forEach(function (alertEl) {
+    const btnCloseAlertEl = alertEl.querySelector(`[${dataElementAttr}="btn-close-alert"]`)
+
+    if (btnCloseAlertEl) {
+        btnCloseAlertEl.addEventListener(`click`, function (event) {
+            event.preventDefault()
+            alertEl.remove()
+        })
+    }
+})
