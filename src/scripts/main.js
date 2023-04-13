8172af0859a268217b46a10000a6b54d053cc23d
Davina Leong <leong.shi.yun@gmail.com>
2023-04-13 17:34:35 +0800

    Able to toggle option active state

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 console.log('main.js loaded')
 
+const classActive = `active`
 const classDNone = `d-none`
+
 const dataElementAttr = `data-element`
+const dataActiveAttr = `data-active`
 
 const alertEls = document.querySelectorAll(`[data-element="alert"]`)
+const btnOptionEls = document.querySelectorAll(`[data-element="btn-option"]`)
 
 alertEls.forEach(function (alertEl) {
     const btnCloseAlertEl = alertEl.querySelector(`[${dataElementAttr}="btn-close-alert"]`)
@@ -14,4 +18,12 @@ alertEls.forEach(function (alertEl) {
             alertEl.remove()
         })
     }
+})
+
+btnOptionEls.forEach(function (btnOptionEl) {
+    btnOptionEl.classList.remove(classActive)
+    btnOptionEl.addEventListener(`click`, function(event) {
+        event.preventDefault()
+        btnOptionEl.toggleAttribute(dataActiveAttr, true)
+    })
 })
